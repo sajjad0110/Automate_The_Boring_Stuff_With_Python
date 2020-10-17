6ce45d94dc9c50176c0603257a46a64a9cba7f14
Shazzadur Rahaman <42701285+sajjad0110@users.noreply.github.com>
2020-10-18 00:10:45 +0600

    Create ex09.py

---
+++ chapter02/exercises/ex09.py
@@ -0,0 +1,11 @@
+# If spam = 1: say 'Hello'
+# else if spam = 2: say 'Howdy'
+# Everything else: say Greetings
+spam = int(input())
+
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
